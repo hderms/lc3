1f73ebc143993b9bd6035b92283d7828ea72ce37
Dermot Haughey <hderms@gmail.com>
2021-02-06 14:13:09 -0600

    only compile instruction builder functions in test

--- src/instruction_builder.rs
+++ src/instruction_builder.rs
@@ -1,9 +1,11 @@
 use crate::registers::RegisterName;
-    pub fn increment(register: RegisterName) -> u16 {
-        let r_value = register as u16;
-        (1 << 12) | ((r_value) << 9) | ((r_value) << 6) | 1 << 5 | 1
-    }
+#[cfg(test)]
+pub fn increment(register: RegisterName) -> u16 {
+    let r_value = register as u16;
+    (1 << 12) | ((r_value) << 9) | ((r_value) << 6) | 1 << 5 | 1
+}
 
-    pub fn add(source_1: RegisterName, source_2: RegisterName, dest: RegisterName) -> u16 {
-        (1 << 12) | ((dest as u16) << 9) | ((source_1 as u16) << 6) | 0 << 5 | source_2 as u16
-    }
+#[cfg(test)]
+pub fn add(source_1: RegisterName, source_2: RegisterName, dest: RegisterName) -> u16 {
+    (1 << 12) | ((dest as u16) << 9) | ((source_1 as u16) << 6) | 0 << 5 | source_2 as u16
+}
